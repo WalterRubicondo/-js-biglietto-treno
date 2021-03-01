cf2d007263bb90222af754e8fbfefac47fe5f7bb
Walter <walterrubicondo@gmail.com>
2021-03-01 16:07:26 +0100

    Fine bonus.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 alert("Benvenuto su BooleanTrain. Prego inserire i dati richiesti per calcolore il prezzo del Suo del biglietto");
 
+var codiceFiscale = prompt("Prego inserisca il Suo Codice Fiscale");
+
+document.getElementById('cf') .innerHTML = "Buon Viaggio " + codiceFiscale;
+
 var chilometri = parseFloat(prompt("Numero di Km per cui desidera viaggiare?"));
 
 var eta = parseInt(prompt("Età?"));
@@ -12,11 +16,16 @@ var biglietto = chilometri * prezzo;
 
 if (eta < 18) {
   // console.log(biglietto - ((biglietto * 20) /100) + " €");
-  alert("Il totale del suo biglietto è pari a " + (biglietto - ((biglietto * 20) /100) + "€"));
-} else if (eta > 65) {
+  // alert("Il totale del suo biglietto è pari a " + (biglietto - ((biglietto * 20) /100)).toFixed(2) + "€");
+  document.getElementById('totalebiglietto') .innerHTML = "Totale: " + (biglietto - ((biglietto * 20) /100)).toFixed(2) + "€";
+} else if (eta >= 65) {
   // console.log(biglietto - (biglietto * 40) /100 + " €");
-  alert("Il totale del suo biglietto è pari a " + (biglietto - (biglietto * 40) /100 + "€"))
+  // alert("Il totale del suo biglietto è pari a " + (biglietto - ((biglietto * 40) /100)).toFixed(2) + "€");
+    document.getElementById('totalebiglietto') .innerHTML = "Totale: " + (biglietto - ((biglietto * 40) /100)).toFixed(2) + "€";
 } else {
   // console.log(biglietto + " €");
-  alert("Il totale del suo biglietto è pari a " + biglietto + "€")
+  // alert("Il totale del suo biglietto è pari a " + biglietto.toFixed(2) + "€");
+    document.getElementById('totalebiglietto') .innerHTML = "Totale: " + biglietto.toFixed(2) + "€";
 }
+
+alert("Si prega di stampare il biglietto");
